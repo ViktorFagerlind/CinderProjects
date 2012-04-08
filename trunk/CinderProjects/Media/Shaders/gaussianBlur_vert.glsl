9dcdca8b3e236aa7acff9bd91010f8de5e0f037e
viktor_fagerlind@hotmail.com <viktor_fagerlind@hotmail.com@912f2d48-f3f0-11de-99d0-77da0b7ae2c6>
2012-04-08 09:38:31 +0000

    Fixed shader

--- trunk/CinderProjects/Media/Shaders/gaussianBlur_vert.glsl
+++ trunk/CinderProjects/Media/Shaders/gaussianBlur_vert.glsl
@@ -3,13 +3,18 @@ varying vec2 vTexCoord;
 // remember that you should draw a screen aligned quad
 void main(void)
 {
-   gl_Position = ftransform();;
+	gl_TexCoord[0] = gl_MultiTexCoord0;
+	gl_Position = ftransform();
+
+/*
+  gl_Position = ftransform();
   
-   // Clean up inaccuracies
-   vec2 Pos;
-   Pos = sign(gl_Vertex.xy);
+  // Clean up inaccuracies
+  vec2 Pos;
+  Pos = sign(gl_Vertex.xy);
  
-   gl_Position = vec4(Pos, 0.0, 1.0);
-   // Image-space
-   vTexCoord = Pos * 0.5 + 0.5;
+  gl_Position = vec4(Pos, 0.0, 1.0);
+  // Image-space
+  vTexCoord = Pos * 0.5 + 0.5;
+*/
 }
