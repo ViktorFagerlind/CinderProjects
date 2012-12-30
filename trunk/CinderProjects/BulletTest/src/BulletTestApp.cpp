fa72fd08abe2a8efeeee08c013c3a528cba0700e
viktor_fagerlind@hotmail.com <viktor_fagerlind@hotmail.com@912f2d48-f3f0-11de-99d0-77da0b7ae2c6>
2012-12-30 11:46:37 +0000

    Updated MSA and got demo to work

--- trunk/CinderProjects/BulletTest/src/BulletTestApp.cpp
+++ trunk/CinderProjects/BulletTest/src/BulletTestApp.cpp
@@ -1,11 +1,13 @@
 #include "cinder/app/AppBasic.h"
 #include "cinder/gl/gl.h"
 #include "cinder/Rand.h"
-#include "cinder/Camera.h"
-
 
 #include "MSABulletRigidBody.h"
 #include "MSABulletRigidWorld.h"
+
+#include "ShaderHelper.h"
+#include "PhongMaterial.h"
+#include "MovingCamera.h"
 
 using namespace ci;
 using namespace ci::app;
@@ -17,23 +19,26 @@ class BulletTestApp : public AppBasic {
 public:
 	void setup();
 	void mouseDown( MouseEvent event );	
+  void keyDown (KeyEvent event);
 	void update();
 	void draw();
 
 private:
-  CameraPersp mCam;
+  shared_ptr<MovingCamera>  mCamera;
+  shared_ptr<PhongMaterial> mMaterial;
 
+  bool mDebugMode;
+
+  uint32_t frameCount;
 };
 
 void BulletTestApp::setup()
 {
-  world.setup(Vec3f(0, 0, 0), Vec3f(getWindowWidth(), getWindowHeight(), 0));
-
+  world.setup(Vec3f(-10, -10, -10), Vec3f(10, 10, 10));
 	{
-		
 		//create a dynamic rigidbody
 		//btCollisionShape* colShape = new btBoxShape(btVector3(1,1,1));
-		btCollisionShape* colShape = new btSphereShape(btScalar(10.));
+		btCollisionShape* colShape = new btSphereShape(btScalar(1.));
 		//	collisionShapes.push_back(colShape);
 		
 		/// Create Dynamic Objects
@@ -48,7 +53,7 @@ void BulletTestApp::setup()
 		if (isDynamic)
 			colShape->calculateLocalInertia(mass,localInertia);
 		
-    startTransform.setOrigin(btVector3(getWindowWidth() /2,getWindowHeight()/2,0));
+    startTransform.setOrigin (btVector3 (0,0,0));
 		
 		//using motionstate is recommended, it provides interpolation capabilities, and only synchronizes 'active' objects
 		btDefaultMotionState* myMotionState = new btDefaultMotionState(startTransform);
@@ -58,14 +63,8 @@ void BulletTestApp::setup()
 		world.addBulletRigidBody(body);
 	}
 	
-	Vec3f p(700, 200, 0);
-	world.createRigidSphere(p, 10);
-
-
-  glEnable(GL_COLOR_MATERIAL);
-  glColorMaterial(GL_FRONT, GL_AMBIENT_AND_DIFFUSE);
-
-	glEnable( GL_LIGHTING );
+	Vec3f p(0, 0, 0);
+  world.createRigidBox (Vec3f(0.f,-15.f,0.f), Vec3f(15.f, 0.3f, 15.f), 0.f);
 
   float white[] = {1.0f, 1.0f, 1.0f , 0.0f};
   float red[]   = {1.0f, 0.0f, 0.0f , 0.0f};
@@ -80,15 +79,44 @@ void BulletTestApp::setup()
   glLightfv(GL_LIGHT0, GL_AMBIENT,  white);
 	glLightfv(GL_LIGHT0, GL_POSITION, p1);
 	glEnable (GL_LIGHT0);
+
+  mDebugMode = false;
+
+  gl::enableDepthRead ();
+  gl::enableDepthWrite ();
+
+  mMaterial.reset (new PhongMaterial (ShaderHelper::loadShader ("../Media/Shaders/phong_vert.glsl", 
+                                                                "../Media/Shaders/phong_frag.glsl"), // Shader
+                                      ColorAf (0.05f, 0.1f,  0.05f, 1.0f),                           // matAmbient,
+                                      ColorAf (0.8f,  0.5f,  0.2f,  1.0f),                           // matDiffuse,
+                                      ColorAf (0.9f,  0.5f,  0.3f,  1.0f),                           // matSpecular,
+                                      6.0f));                                                        // matShininess
+
+  mCamera.reset (new MovingCamera(50.0f));
+
+  frameCount = 0;
 }
 
 void BulletTestApp::mouseDown( MouseEvent event )
 {
 	for(int i=0; i<10; i++) 
   {
-    Vec3f p(Rand::randInt(0, getWindowWidth()), Rand::randInt(0, getWindowHeight()), 0);
-		world.createRigidSphere(p, Rand::randInt(10, 50));
+    Vec3f p(Rand::randFloat(-10.f, 10.f), Rand::randFloat(-10.f, 10.f), Rand::randFloat(-10.f, 10.f));
+
+		world.createRigidSphere(p, Rand::randFloat(.5f, 1.f));
 	}
+}
+
+void BulletTestApp::keyDown (KeyEvent event)
+{
+  char c = event.getChar();
+
+  if (c == 'd')
+  {
+    mDebugMode = !mDebugMode;
+  }
+
+  mCamera->keyDown (event);
 }
 
 void BulletTestApp::update()
@@ -99,35 +127,53 @@ void BulletTestApp::update()
 void BulletTestApp::draw()
 {
 	// clear out the window with black
-	gl::clear( Color( 0, 0, 0 ) ); 
+	gl::clear (Color (0, 0, 0)); 
 
-  /*
-  Vec3f cameraPosition = Vec3f (0.0f, 0.0f, 1000.0f);
+  // Setup camera
+  mCamera->setModelMatrix ();
 
-  mCam.lookAt(cameraPosition,  // camera
-              Vec3f::zero(),   // center
-              Vec3f::yAxis()); // up
+  if (mDebugMode)
+  {
+    world.debugDraw();
+  }
+  else
+  {
+    mMaterial->bind();
 
-  gl::setMatrices (mCam);
-  */
-  world.debugDraw();
+	  for (int j=0; j<world.getBulletWorld().getNumCollisionObjects(); j++)
+	  {
+		  btCollisionObject* obj = world.getBulletWorld().getCollisionObjectArray()[j];
+		  btRigidBody* body = btRigidBody::upcast(obj);
+		  if (body && body->getMotionState())
+		  {
+			  btTransform trans;
+			  body->getMotionState()->getWorldTransform(trans);
 
-	for (int j=0; j<world.getBulletWorld().getNumCollisionObjects(); j++)
-	{
-		btCollisionObject* obj = world.getBulletWorld().getCollisionObjectArray()[j];
-		btRigidBody* body = btRigidBody::upcast(obj);
-		if (body && body->getMotionState())
-		{
-			btTransform trans;
-			body->getMotionState()->getWorldTransform(trans);
+			  glPushMatrix();
+			  glTranslatef(trans.getOrigin().getX(), trans.getOrigin().getY(), trans.getOrigin().getZ());
 
-			glPushMatrix();
-			glTranslatef(trans.getOrigin().getX(), trans.getOrigin().getY(), trans.getOrigin().getZ());
-			float radius = ((btSphereShape*) body->getCollisionShape())->getRadius();
-      gl::drawSphere (Vec3f(0,0,0), radius); 
-			glPopMatrix();
-		}
-	}
+        btCollisionShape *shape = body->getCollisionShape();
+
+        switch (shape->getShapeType ())
+        {
+        case 0:
+          gl::drawCube (Vec3f(0,0,0), btVector3_To_Vec3f (((btBoxShape*) shape)->getHalfExtentsWithoutMargin())); 
+          break;
+        case 8:
+          gl::drawSphere (Vec3f(0,0,0), ((btSphereShape*) shape)->getRadius()); 
+          break;
+        }
+
+			  glPopMatrix();
+		  }
+	  }
+
+    mMaterial->unbind();
+  }
+
+  frameCount++;
+  if ((frameCount % 10) == 0)
+    console() << "FPS: " << getAverageFps() << std::endl;
 }
 
 CINDER_APP_BASIC( BulletTestApp, RendererGl )
--- trunk/CinderProjects/CinderGraphicsLib/include/Macros.h
+++ trunk/CinderProjects/CinderGraphicsLib/include/Macros.h
@@ -1,1 +1,3 @@
+#pragma once
+
 #define NOF_ELEMENTS(ARRAY) ((uint32_t)(sizeof(ARRAY)/sizeof(ARRAY[0])))
